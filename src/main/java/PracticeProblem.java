309d7b92c521e2dcf99176a9608cb4e2fae32474
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-07 10:07:25 -0400

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,36 @@
-public class PracticeProblem {
+public class PracticeProblem{
+        static String[][] maze;
+        static int paths;
+        static int search(int row, int col){
+                if(maze[row][col].equals("F")){
+                        paths++;
+                        return 0;
+                }
 
-	public static void main(String args[]) {
+                int min = maze.length * maze[row].length + 1;
+                if(row-1 > -1){
+                        int candidate = search(row-1, col);
+                        min = candidate < min ? candidate : min;
+                }
+                if(col+1 < maze[row].length){
+                        int candidate = search(row, col+1);
+                        min = candidate < min ? candidate : min;
+                }
+                return min+1;
+        }
 
-	}
+        static int searchMazeMoves(String[][] arr){
+                maze = arr;
+                paths = 0;
+                return search(arr.length-1, 0);
+        }
 
-	
+        static int noOfPaths(String[][] arr){
+                maze = arr;
+                paths = 0;
+                search(arr.length-1, 0);
+                return paths;
+        }
 
+        public static void main(String args[]){}
 }
